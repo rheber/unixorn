64e04925fc44f2375533ffd171fd5e37714e3564
rheber <rheber@rediffmail.com>
2021-03-21 14:24:15 +1100

    arrow keys

--- src/Unixorn.tsx
+++ src/Unixorn.tsx
@@ -4,18 +4,35 @@ import { UnixornConfiguration } from './types';
 
 const Unixorn: React.FunctionComponent<UnixornConfiguration> = props => {
   const [inputPreCursor, setInputPreCursor] = useState("");
-  const [inputPostCursor] = useState("");
+  const [inputPostCursor, setInputPostCursor] = useState("");
   const prompt = props.prompt || "> "
 
   const handleKeyDown = useCallback((e: React.KeyboardEvent<HTMLDivElement>) => {
     e.preventDefault();
+    if (e.key === "ArrowLeft") {
+      if (inputPreCursor.length > 0) {
+        const lastChar = inputPreCursor.slice(-1);
+        setInputPreCursor(inputPreCursor.slice(0, -1));
+        setInputPostCursor(lastChar + inputPostCursor);
+      }
+    }
+    if (e.key === "ArrowRight") {
+      if (inputPostCursor.length > 0) {
+        const firstChar = inputPostCursor[0];
+        setInputPostCursor(inputPostCursor.substr(1));
+        setInputPreCursor(inputPreCursor + firstChar);
+      }
+    }
     if (e.key === "Backspace") {
       setInputPreCursor(inputPreCursor.slice(0, -1));
+    }
+    if (e.key === "Delete") {
+      setInputPostCursor(inputPostCursor.substr(1));
     }
     if (e.key.length === 1) {
       setInputPreCursor(inputPreCursor + e.key);
     }
-  }, [inputPreCursor]);
+  }, [inputPreCursor, inputPostCursor]);
 
   return (
     <div
