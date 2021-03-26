29760d4463d82d9631a5a0eb59191bc71dbf0c5d
rheber <rheber@rediffmail.com>
2021-03-26 20:30:48 +1100

    Updates

---
+++ precache-manifest.c62c94b7f603d5b3edce657734485284.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c563af7773b7907201c312c28937524e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea57c01f9d5c59ac5951",
+    "url": "./static/css/main.457f3081.chunk.css"
+  },
+  {
+    "revision": "756b5c4a378fbf25684d",
+    "url": "./static/js/2.31bb8e90.chunk.js"
+  },
+  {
+    "revision": "81e55dd6344646c2e450a0c59ee381b8",
+    "url": "./static/js/2.31bb8e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea57c01f9d5c59ac5951",
+    "url": "./static/js/main.dcf8d41d.chunk.js"
+  },
+  {
+    "revision": "59bd054f3bde6b0e219b610549d13f6f",
+    "url": "./static/js/main.dcf8d41d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b02c5c18a9fb5e693018",
+    "url": "./static/js/runtime-main.8143a272.js"
+  }
+]);
